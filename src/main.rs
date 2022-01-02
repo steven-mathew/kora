56117cd59fbcb347a1a18a84624f0ad343b444f9
Steven Mathew <ste.tho.mat@gmail.com>
2022-01-02 18:18:21 -0500

    to match

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,19 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if c == 'q' {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == b'q' & 0b0001_1111 {
+                    break;
+                }
+            }
+            Err(err) => panic!("the world exploded: {}", err),
         }
     }
 }
